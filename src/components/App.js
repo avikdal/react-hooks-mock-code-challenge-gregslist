11371cfeb19276a7ca0aea4b6bd39c6e400fdca5
amanda vikdal <vikdal.amanda@gmail.com>
2023-02-23 11:02:01 -0700

    commit tree 2dce9f7998f92af8f395d2eee79c13f9461b3edc

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [search, setSearch] = useState("")
+
+  function handleSearch(currentSearch) {
+    setSearch(currentSearch)
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header search={search} handleSearch={handleSearch} />
+      <ListingsContainer search={search} />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,48 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+
+function ListingsContainer({ search }) {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then((r) => r.json())
+    .then(items => setItems(items))
+  }, [])
+
+  function handleDelete(id){
+    console.log("handle delete item id in container", id)
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE"
+    })
+    .then(r => r.json())
+    .then(() => {
+      const remainingItems = items.filter(item => item.id !== id)
+      setItems(remainingItems)
+    })
+  }
+
+  const searchedListings = items.filter((item) => {
+    return item.description.toLowerCase().includes(search.toLowerCase())
+  })
+  console.log("searchedListings", searchedListings)
+
+  const listingCards = searchedListings.map((item) => (
+      <ListingCard 
+        key={item.id}  
+        id={item.id}
+        description={item.description} 
+        image={item.image} 
+        location={item.location}
+        onDeleteClick={handleDelete}
+      />
+  ))
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
